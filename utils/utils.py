0586d528ce10370028f7108a08c062d0b912f42e
Romulus <33386148+jhy354@users.noreply.github.com>
2022-10-09 15:26:46 +0800

    add files in ./utils/

--- utils/utils.py
+++ utils/utils.py
@@ -1,1 +1,11 @@
+class Debug:
+    """
+    Debug log print
+    """
+    def __init__(self, debug_mode = False):
+        self.debug_mode = debug_mode
 
+    def __lshift__(self, other):
+        if self.debug_mode:
+            print(other, end=" ")
+            return self
