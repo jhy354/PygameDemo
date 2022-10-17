bbe6cc39efd47ebc7bdf4f4560210c85ad2e6e5e
Romulus <33386148+jhy354@users.noreply.github.com>
2022-10-17 16:03:08 +0800

    Add files via upload

--- utils/utils.py
+++ utils/utils.py
@@ -1,11 +1,18 @@
+LAST_LINE = ""
+
+
 class Debug:
     """
     Debug log print
     """
-    def __init__(self, debug_mode = False):
+
+    def __init__(self, debug_mode=False):
         self.debug_mode = debug_mode
 
     def __lshift__(self, other):
-        if self.debug_mode:
-            print(other, end=" ")
-            return self
+        global LAST_LINE
+        if self.debug_mode and str(other) != LAST_LINE:
+            print(other, end="")
+            LAST_LINE = str(other)
+
+        return self
